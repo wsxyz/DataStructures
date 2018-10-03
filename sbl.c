00dc5a3b7e7dd539ad9a7a8f435e30dd6847b46c
WSX <belaev91@gmail.com>
2018-10-03 08:29:35 +0200

    feat: Implementation of SBL_create function
    The SBL_create() function has been implemented in the sbl.c file.

--- sbl.c
+++ sbl.c
@@ -4,3 +4,34 @@
 //
 
 #include "sbl.h"
+
+Sbl SBL_create() {
+
+    Sbl l;
+    l.head = (Node *) malloc(sizeof(Node));
+
+    if (l.head == NULL) {
+        printf("Create Error: Malloc Failed\n");
+    } else {
+        l.last = (Node *) malloc(sizeof(Node));
+        if (l.last == NULL) {
+            printf("Create Error: Malloc Failed\n");
+            free(l.head);
+        } else {
+            l.size = (long *) malloc(sizeof(long));
+            if (l.size == NULL) {
+                printf("Create Error: Malloc Failed\n");
+                free(l.head);
+                free(l.last);
+            } else {
+                *(l.size) = 0;
+                l.curr = l.last;
+                (*l.head).next = l.last;
+                (*l.head).prev = NULL;
+                (*l.last).prev = l.head;
+                (*l.last).next = NULL;
+            }
+        }
+    }
+    return l;
+}
