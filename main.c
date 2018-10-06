92582a86c088b336d3cc347346952a3411101373
WSX <belaev91@gmail.com>
2018-10-06 16:56:40 +0200

    test: Added Test for Consult Verification
    A test designed to verify if the first and last element are the expected
    has been added to the main.c file.

--- main.c
+++ main.c
@@ -51,8 +51,53 @@ int testInsertion() {
     return 0;
 }
 
+/**
+ * @Name: testConsult
+ * @Def: A test function designed to verify if the first and the last element are the expected
+ * @Args: ----.
+ * @Ret: Returns 0 if this test reaches successfully the return statement.
+ *
+ **/
+int testConsult() {
+
+    Sbl list = SBL_create();
+    Element e;
+    int i = 0;
+    int n = 10;
+    time_t t;
+    srand((unsigned)time(&t));
+
+    printf("\n---- Consult Test ----\n");
+    printf("Number of Elements to Insert: %d\n\n", n);
+
+    for (i = 0; i < n; i++) {
+        e.num = rand() % n;
+        SBL_insertOrdered(&list, e);
+    }
+
+    SBL_goToHead(&list);
+    while (!SBL_isLast(list)) {
+        e = SBL_consult(list);
+        printf("\tElement: %d\n", e.num);
+        SBL_goToNext(&list);
+    }
+
+    SBL_goToHead(&list);
+    e = SBL_consult(list);
+    printf("\nFirst Element of the List: %d\n", e.num);
+
+    SBL_goToLast(&list);
+    e = SBL_consult(list);
+    printf("First Element of the List: %d\n", e.num);
+
+    SBL_destroy(&list);
+
+    return 0;
+}
+
 int main() {
     printf("Data Structures in C!\n");
     testInsertion();
+    testConsult();
     return 0;
 }
